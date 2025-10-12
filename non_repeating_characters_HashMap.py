e2e1cd7f3ffb2192b7a202032dc4065735dcde94
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2025-10-12 13:01:01 +0530

    non_repeating_characters_HashMap.py

---
+++ non_repeating_characters_HashMap.py
@@ -0,0 +1,10 @@
+def first_non_repeating_character(s):
+    frequency = {}
+    for char in s:
+        frequency[char] = frequency.get(char, 0) + 1
+    for char in s:
+        if frequency[char] == 1:
+            return char
+    return None
+
+print(first_non_repeating_character("hello"))
