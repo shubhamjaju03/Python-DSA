424cd475cefa3da94005429736284a9ce8ebd93f
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2025-10-21 13:29:14 +0530

    Implement dynamic programming for Tribonacci number

---
+++ Nth Tribonacci Number_dp.py
@@ -0,0 +1,13 @@
+def tribonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+
+    dp = [0] * (n + 1)
+    dp[0], dp[1], dp[2] = 0, 1, 1
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+    return dp[n]
