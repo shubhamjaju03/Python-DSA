8259d8d4fa53ce9471952196d44786d817a92f71
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2025-11-10 12:05:24 +0530

    Implement combination generation function

---
+++ Combinations.py
@@ -0,0 +1,12 @@
+def combine(n, k):
+    res = []
+    def backtrack(start, path):
+        if len(path) == k:
+            res.append(path[:])
+            return
+        for i in range(start, n + 1):
+            path.append(i)
+            backtrack(i + 1, path)
+            path.pop()
+    backtrack(1, [])
+    return res
