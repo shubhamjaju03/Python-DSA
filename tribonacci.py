c52ba276a40bcf413bd0bcc2fe387d765103b465
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2025-11-18 12:51:05 +0530

    Create tribonacci.py

---
+++ tribonacci.py
@@ -0,0 +1,12 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n==0:
+            return 0
+        elif n==1 or n==2:
+            return 1
+        
+        t0,t1,t2=0,1,1
+        for _ in range(3,n+1):
+            t_next=t0+t1+t2
+            t0,t1,t2=t1,t2,t_next
+        return t2
