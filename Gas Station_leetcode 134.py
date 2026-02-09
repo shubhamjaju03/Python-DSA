fedf8d4d92d6c4e33b7b00d22b7543b802821814
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2026-02-09 14:20:55 +0530

    Implement gas station circuit completion algorithm

---
+++ Gas Station_leetcode 134.py
@@ -0,0 +1,19 @@
+def canCompleteCircuit(gas, cost):
+    total_gas = 0
+    total_cost = 0
+    tank = 0
+    start = 0
+
+    for i in range(len(gas)):
+        total_gas += gas[i]
+        total_cost += cost[i]
+        tank += gas[i] - cost[i]
+
+        if tank < 0:
+            start = i + 1
+            tank = 0
+
+    if total_gas < total_cost:
+        return -1
+
+    return start
